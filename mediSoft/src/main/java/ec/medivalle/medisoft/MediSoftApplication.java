2696031ed57478885fa90df77798500547704f18
opizarro98 <131551382+opizarro98@users.noreply.github.com>
2023-09-25 16:03:19 -0500

    first Commit

---
+++ mediSoft/src/main/java/ec/medivalle/medisoft/MediSoftApplication.java
@@ -0,0 +1,13 @@
+package ec.medivalle.medisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSoftApplication.class, args);
+	}
+
+}
